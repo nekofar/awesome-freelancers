d9880bab6bfd43e496a11c1bf2d92d7ed95490f9
Milad Nekofar <milad@nekofar.com>
2019-10-06 13:26:52 +0330

    Add some extra information to the source file

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const source = require("./source.json");
 const people = require("../people.json");
 const {shuffle} = require('lodash');
 
-const tableIndex = 3;
+const tableIndex = 5;
 const sourceJson = () => {
     let head = source[tableIndex].table.headers;
     let rows = shuffle(people);
